c475801fb75ef1ff6c093e87b748a60b792dec64
Sviatoslav Hrabar <36728685+Sviat1553@users.noreply.github.com>
2023-06-26 18:04:26 +0300

    Added the optional feature: one author is able to post no more than 1 post per 10 minutes

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Cookie;
 use App\Models\Post;
+use Carbon\Carbon;
 
 class PostController extends Controller
 {
@@ -29,6 +32,14 @@ class PostController extends Controller
         ]);
 
         $userId = $request->cookie('user_id') ?? Str::random(32);
+
+        // Знайти останній допис цього користувача
+        $lastPost = Post::where('user_id', $userId)->orderBy('created_at', 'desc')->first();
+
+        if($lastPost && $lastPost->created_at->gt(Carbon::now()->subMinutes(10))) {
+            return redirect()->route('posts.index')->with('error', 'You can only post once every 10 minutes.');
+        }
+
         Cookie::queue('user_id', $userId, 60 * 24 * 365);  // Зберігаємо ідентифікатор користувача на рік
 
         $post = new Post([
@@ -38,7 +49,7 @@ class PostController extends Controller
 
         $post->save();
 
-        return redirect()->route('posts.index');
+        return redirect()->route('posts.index')->with('success', 'Post created successfully.');
     }
 
     public function destroy(Post $post)
